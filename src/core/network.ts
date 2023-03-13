d39cf977894f1425f1b61534041ca6f696f677a5
Ricky <rickyzoe0722@gmail.com>
2023-03-13 16:47:36 +0800

    add get & post sample.

--- src/core/network.ts
+++ src/core/network.ts
@@ -32,12 +32,31 @@ export class NetWork {
         const port : number = httpconf ["port"];
 
         var app = express()
+
+        app.use(express.urlencoded({
+            extended: false
+         }))
+
         app.get('/', function (req, res) {
-        res.send('Hello World!')
+          res.send(`
+            <form action="/answer" method="POST">
+            <p>猜猜看，我喜歡什麼顏色的衣服？</p>
+            <input name="preferColor" autocomplete="off">
+            <button>送出</button>
+            </form>`)
+        })
+
+        app.post('/answer', function (req, res) {
+            if (req.body.preferColor == "紅色") {
+                res.send("答對了，你真暸解我")
+            } else {
+                res.send("對不起，不是這個答案")
+            }
+            res.send("感謝送出表單")
         })
 
         app.listen(port, function () {
-        console.log('Example app listening on port 8000!')
+        console.log(`Example app listening on port ${port}!`)
         })
     }
 }
