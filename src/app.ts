af48a9d9624c0044cc17032cad987c5fb2aa9996
Ricky <rickyzoe0722@gmail.com>
2023-03-17 10:44:27 +0800

    add example4 source code

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,16 @@ import { NetWork } from "./core/network"
 import { Example1 } from "./examples/example1";
 import { Example2 } from "./examples/example2";
 import { Example3 } from "./examples/example3";
+import { Example4 } from "./examples/example4";
 
 export class App {
-    private exsample_mode: number = -1;
-
-    constructor () {
-      this.exsample_mode = Config.getInstance().getParam ("exsample_mode");
-    }
-
     protected getExample(): IExample {
-      switch(this.exsample_mode) {
+      let mode: number = Config.getInstance().getParam ("exsample_mode")
+      switch(mode) {
         case 1: return new Example1();
         case 2: return new Example2();
         case 3: return new Example3();
+        case 4: return new Example4();
       }
     }
 
---
+++ src/examples/example4.ts
@@ -0,0 +1,28 @@
+import { Config } from "../config"
+import { IExample } from "../core/base";
+
+export class Example4 extends IExample {
+    run(): void {
+        const httpconf = Config.getInstance().get()["portal"];
+        const port : number = httpconf ["port"];
+
+        const app = require('express')()
+
+        // Middleware 中間件 (通常會被使用管理流程的控制).
+        // middleware是會按順序執行的，所以在設定app.use的時候，要注意上下的順序.
+        // Express.js 傳入的 next 參數，表示「進行下一個流程」的意思，
+        // 下面一個簡單的例子，當該流程的方法執行完之後，則進行下一個流程(這邊用nextFunc來示範).
+        app.get('/hello', function (req: any, res: any, next: any) {
+            console.log(req.query);
+            next();
+        }, this.nextFunc);
+
+        app.listen(port, function () {
+           console.log(`Example app listening on port ${port}!`)
+        })
+    }
+    
+    nextFunc() {
+        console.log(`the function is test.`);
+    }
+}
